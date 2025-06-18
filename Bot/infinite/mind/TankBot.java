29a39f31e843b75e0bb42afe9adcd9f0130ad073
ZxNashx <axylschulz0@gmail.com>
2025-06-18 15:46:08 -0600

    redo with simple movements

--- Bot/infinite/mind/TankBot.java
+++ Bot/infinite/mind/TankBot.java
@@ -3,8 +3,6 @@ package infinite.mind;
 import dev.robocode.tankroyale.botapi.*;
 import dev.robocode.tankroyale.botapi.events.*;
 import java.awt.Color;
-import java.util.Random;
-
 
 /**
  * <p>Robocode API functions used in this file:</p>
@@ -27,16 +25,10 @@ import java.util.Random;
  * </ul>
  */
 
- 
 /**
  * Basic tank bot demonstrating simple movement and firing logic.
  */
 public class TankBot extends Bot {
-
-    /**
-     * Locator used to scan for enemies and handle firing logic.
-     */
-    private final TargetLocator locator = new TargetLocator();
 
     /**
      * Distance to the last scanned opponent. Used to decide fire power.
@@ -66,93 +58,81 @@ public class TankBot extends Bot {
      *
      */
 
-@Override
-public void run() {
-    double arenaWidth = getArenaWidth();
-    double arenaHeight = getArenaHeight();
-    double centerX = arenaWidth / 2.0;
-    double centerY = arenaHeight / 2.0;
+    @Override
+    public void run() {
+        double arenaWidth = getArenaWidth();
+        double arenaHeight = getArenaHeight();
+        double centerX = arenaWidth / 2.0;
+        double centerY = arenaHeight / 2.0;
 
-    // Set colors
-    setBodyColor(Color.cyan);
-    setTurretColor(Color.darkGray);
-    setRadarColor(Color.green);
-    setBulletColor(Color.orange);
-    setScanColor(Color.pink);
+        // Set colors
+        setBodyColor(Color.cyan);
+        setTurretColor(Color.darkGray);
+        setRadarColor(Color.green);
+        setBulletColor(Color.orange);
+        setScanColor(Color.pink);
 
-    while (isRunning()) {
-
-        System.out.println("Distance to center: " + getDistance(getX(), getY(), centerX, centerY));
-        System.out.println("Target distance for middle:" + Math.min(arenaWidth, arenaHeight) / 2.0);
-        if(getDistance(getX(), getY(), centerX, centerY) > Math.min(arenaWidth, arenaHeight) / 2.0){
-            locator.findTarget(this);
-            if(getX() > centerX){
-                moveLeft(getX() - centerX);
-            }else{
-                moveRight(centerX - getX());
+        while (isRunning()) {
+            System.out.println("Distance to center: " + getDistance(getX(), getY(), centerX, centerY));
+            // move near the center of the arena if we are too far away
+            if (getDistance(getX(), getY(), centerX, centerY) > Math.min(arenaWidth, arenaHeight) / 2.0) {
+                if (getX() > centerX) {
+                    moveLeft(getX() - centerX);
+                } else {
+                    moveRight(centerX - getX());
+                }
+                if (getY() > centerY) {
+                    moveDown(getY() - centerY);
+                } else {
+                    moveUp(centerY - getY());
+                }
             }
-            if(getY() > centerY){
-                moveDown(getY() - centerY);
-            }else{
-                moveUp(centerY - getY());
-            }
+
+            printDebugInfo();
+        }
+    }
+
+    private double getDistance(double p1x, double p2x, double p1y, double p2y) {
+        return Math.sqrt((p2x - p1x) * (p2x - p1x) + (p2y - p1y) * (p2y - p1y));
+    }
+
+    private void moveUp(double distance) { // 90°
+        double direction = getDirection();
+        if (direction > 90) {
+            turnRight(direction - 90);
+        } else {
+            turnLeft(90 - direction);
+        }
+        forward(distance);
+    }
+
+    private void moveLeft(double distance) { // 180°
+        double direction = getDirection();
+
+        if (direction < 180) {
+            turnRight(direction - 180);
+        } else {
+            turnLeft(180 - direction);
         }
 
-        printDebugInfo();
-        locator.findTarget(this);
-    }
-}
-
-private double getDistance(double p1x, double p2x, double p1y, double p2y) {
-    return Math.sqrt((p2x - p1x) * (p2x - p1x) + (p2y - p1y) * (p2y - p1y));
-}
-
-private void moveUp(double distance){ // 90°
-    double direction = getDirection();
-    if(direction > 90){
-        turnRight(direction - 90);
-        locator.turning(direction - 90);
-    }else{
-        locator.turning(-(direction - 90));
-        turnLeft(90 - direction);
-    }
-    forward(distance);
-}
-
-private void moveLeft(double distance){ // 180°
-    double direction = getDirection();
-
-    if(direction < 180){
-        turnRight(direction - 180);
-        locator.turning(direction - 180);
-    }else{
-        turnLeft(180 - direction);
-        locator.turning(-(180 - direction));
+        forward(distance);
     }
 
-    forward(distance);
-}
+    private void moveDown(double distance) { // 270°
+        double direction = getDirection();
+        if (direction < 270) {
+            turnRight(direction - 270);
+        } else {
+            turnLeft(270 - direction);
+        }
 
-private void moveDown(double distance){ // 270°
-    double direction = getDirection();
-
-    if(direction < 270){
-        turnRight(direction - 270); 
-        locator.turning(direction - 270);
-    }else{
-        turnLeft(270 - direction);
-        locator.turning(-(270 - direction));
+        forward(distance);
     }
 
-    forward(distance);
-}
-
-    private void moveRight(double distance){
+    private void moveRight(double distance) {
         double direction = getDirection();
         // if we want to move right, we want the angle to be set at 0
         turnRight(direction);
-        locator.turning(direction);
-
         forward(distance);
     }
 
@@ -164,16 +144,6 @@ private void moveDown(double distance){ // 270°
      */
     @Override
     public void onScannedBot(ScannedBotEvent e) {
-        System.out.println("Certainty: " + locator.getCertainty(getDirection()));
-        locator.updateOnScan(e);
-        opponentDistance = distanceTo(e.getX(), e.getY());
-        if(opponentDistance < 10 && getGunHeat() == 0){
-            fire(3);
-        }else{
-            if(locator.getCertainty(getDirection()) > 3 && getGunHeat() == 0){
-                fire(Math.min(Math.round(locator.getCertainty(getDirection()) / 10.0) * 4, 3));
-            }
-        }
 
     }
 
@@ -188,27 +158,19 @@ private void moveDown(double distance){ // 270°
         // Calculate the bearing to the direction of the bullet
         double bearing = calcBearing(e.getBullet().getDirection());
 
-        // Turn 90 degrees to the bullet direction based on the bearing
-        turnRight(bearing + 91);
-
-        // let the locator know we are moving
-        locator.turning(bearing + 91);
-        locator.moving();
-        forward(10);
     }
 
     /**
      * Outputs the bot's current position and energy to the console.
      *
      */
-    private void printDebugInfo(){
-            System.out.println("Debug Info:");
-            System.out.println("  X: " + getX() + ", Y: " + getY());
-            System.out.println("  Energy: " + getEnergy());
-            System.out.println("  Opponent Distance: " + opponentDistance);
-            System.out.println("  Gun Heat: " + getGunHeat());
-            System.out.println("  Facing Direction: " + getDirection());
+    private void printDebugInfo() {
+        System.out.println("Debug Info:");
+        System.out.println("  X: " + getX() + ", Y: " + getY());
+        System.out.println("  Energy: " + getEnergy());
+        System.out.println("  Opponent Distance: " + opponentDistance);
+        System.out.println("  Gun Heat: " + getGunHeat());
+        System.out.println("  Facing Direction: " + getDirection());
     }
-
 
 }
